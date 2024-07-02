821ab4172e4a649c39c935b493443e9f1763b1be
Vadim Korolev <vkorol98@gmail.com>
2024-07-02 18:43:49 +0300

    Fix

--- tests/homework.py
+++ tests/homework.py
@@ -7,7 +7,7 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-    if current_time.hour >= 22 or current_time.hour <= 6:
+    if current_time.hour >= 22 or current_time.hour < 6:
         is_dark_theme = True
     else:
         is_dark_theme = False
@@ -30,7 +30,7 @@ def test_dark_theme_by_time_and_user_choice():
 
 def is_dark_theme(current_time=time(hour=16), dark_theme_enabled_by_user=True):
     if dark_theme_enabled_by_user is None:
-        if current_time.hour >= 22 or current_time.hour <= 6:
+        if current_time.hour >= 22 or current_time.hour < 6:
             return True
         else:
             return False
